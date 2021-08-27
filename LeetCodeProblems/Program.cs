e8629f9a5ef70915f98790dc4245dc033b89a2ec
Helen <hlin@c4itrgtech.com>
2021-08-26 18:33:31 -0600

    Initial commit

---
+++ LeetCodeProblems/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LeetCodeProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string changedString = Interpret("GGGGG()(al)()(al)()(al)");
+            Console.WriteLine(changedString);
+        }
+
+        public static string Interpret(string command)
+        {
+            return command.Replace("()", "o").Replace("(al)", "al");
+        }
+    }
+}
